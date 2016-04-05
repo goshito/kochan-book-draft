1ff20c98a29ffc04656bf75b50861df66aed4a92
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-05 13:01:14 +0300

    Chapter 8, Exercise 16 Modify Program 8.14 so that the user can convert any number of integers. Make provision for the program to terminate when a zero is typed in as the value of the number to be converted.

---
+++ main.c
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: georgi
+ *
+ * Created on March 31, 2016, 3:48 PM
+ */
+
+/* A matrix M with i rows, j columns can be transposed into a matrix N having j rows
+and i columns by simply setting the value of N a,b equal to the value of M b,a for all
+relevant values of a and b.
+a. Write a function transposeMatrix that takes as an argument a 4 x 5 matrix
+and a 5 x 4 matrix. Have the function transpose the 4 x 5 matrix and store
+the results in the 5 x 4 matrix. Also write a main routine to test the function.
+b. Using variable-length arrays, rewrite the transposeMatrix function devel-
+oped in exercise 12a to take the number of rows and columns as arguments,
+and to transpose the matrix of the specified dimensions. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Chapter 8, Exercise 12b
+ */
+/*
+void displayMatrix(int matrix[row][col]);
+void transposeMatrix(int row, int col, int matrix[row][col]);
+ */
+
+// do transpose using function
+
+void transpose(int **m1, int **m2, int row, int col);
+
+int main(int argc, char** argv) {
+    
+    int sampleMatrix[3][3] = {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+    
+    int transposedMatrix[3][3] = {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+    
+    // display the matrix before transpose
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%i ", sampleMatrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    transpose(sampleMatrix, transposedMatrix, 3, 3);
+
+    printf("\n");
+        // display the matrix after transpose
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%i ", transposedMatrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    
+    //display matrix before transpose
+    //displayMatrix(sampleMatrix[3][3]);
+    //transpose matrix
+    //transposeMatrix(m, n, sampleMatrix);
+    //display transposed matrix
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
+// function to transpose the matrix
+// I should try to return transposed matrix
+void transpose(int **m1, int **m2, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            m2[j][i] = m1[i][j];
+        }
+    }    
+}
+
+
+/*
+void displayMatrix(int matrix[row][col]) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("matrix[%d][%d}: %d", i, j, matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void transposeMatrix(int row, int col, int matrix[row][col]) {
+    
+}
+*/
+
