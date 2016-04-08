1997093e9550020d81cd032c96c4727cfc9898dd
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-08 14:13:14 +0300

    Chapter 9, Exercise 2 - solved using functions ver.2(cleaner)  * Write a program that permits the user to type in two dates and then calculates  * the number of elapsed days between the two dates. Try to structure the  * program logically into separate functions. For example, you should have a  * function that accepts as an argument a date structure and returns the values  * of N computed as shown preciously. This function can then be called twice,  * once for each date, and the difference taken to determine the number of  * elapsed days.

--- main.c
+++ main.c
@@ -1,120 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: georgi
- *
- * Created on March 31, 2016, 3:48 PM
- */
-
-/* A matrix M with i rows, j columns can be transposed into a matrix N having j rows
-and i columns by simply setting the value of N a,b equal to the value of M b,a for all
-relevant values of a and b.
-a. Write a function transposeMatrix that takes as an argument a 4 x 5 matrix
-and a 5 x 4 matrix. Have the function transpose the 4 x 5 matrix and store
-the results in the 5 x 4 matrix. Also write a main routine to test the function.
-b. Using variable-length arrays, rewrite the transposeMatrix function devel-
-oped in exercise 12a to take the number of rows and columns as arguments,
-and to transpose the matrix of the specified dimensions. */
-
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * Chapter 8, Exercise 12b
- */
-/*
-void displayMatrix(int matrix[row][col]);
-void transposeMatrix(int row, int col, int matrix[row][col]);
- */
+struct date {
+    int month;
+    int day;
+    int year;
+};
 
-// do transpose using function
+// Function to calculate f(year, month) and return the value of year !!!(Should the function take structure??)
 
-void transpose(int **m1, int row, int col);
-
-int main(int argc, char** argv) {
-    
-    int sampleMatrix[3][3] = {
-        { 1, 2, 3 },
-        { 4, 5, 6 },
-        { 7, 8, 9 }
-    };
-    
-    int transposedMatrix[3][3] = {
-        { 0, 0, 0 },
-        { 0, 0, 0 },
-        { 0, 0, 0 }
-    };
-    
-    // display the matrix before transpose
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%i ", sampleMatrix[i][j]);
-        }
-        printf("\n");
-    }
-    
-    transpose(sampleMatrix, 3, 3);
-
-    printf("\n");
-    /*    
-    //display the matrix after transpose
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%i ", transposedMatrix[i][j]);
-        }
-        printf("\n");
-    }
-    */
-    
-    
-    
-    
-    //display matrix before transpose
-    //displayMatrix(sampleMatrix[3][3]);
-    //transpose matrix
-    //transposeMatrix(m, n, sampleMatrix);
-    //display transposed matrix
-    
-    
-
-    return (EXIT_SUCCESS);
+int f(int year, int month) {
+    if (month <= 2)
+        return year - 1;
+    else
+        return year;
 }
 
-// function to transpose the matrix
-// I should try to return the transposed matrix
-// attempted to display the transposed matrix within the transpose func
-void transpose(int **m1, int row, int col) {
-    int m2[row][col];
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            m2[j][i] = m1[i][j];
-        }
-    }
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++)
-            printf("%i ", m2[i][j]);
-        printf("\n");
-    }
+// Function to calculate g(month) !!!(Should the function take structure??)
+
+int g(int month) {
+    if (month <= 2)
+        return month + 13;
+    else
+        return month + 1;
 }
 
+// Function to calculate N
 
-/*
-void displayMatrix(int matrix[row][col]) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            printf("matrix[%d][%d}: %d", i, j, matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void transposeMatrix(int row, int col, int matrix[row][col]) {
+int calcDays(struct date date) {
+    int n;
     
+    n = 1461 * f(date.year, date.month) / 4 + 153 * g(date.month) / 5 + date.day;
+    
+    return n;
 }
-*/
 
+int main(void) {
+    struct date date1, date2;
+        
+    printf("Enter date #1(mm/dd/yyyy): \n");
+    scanf("%i/%i/%i", &date1.month, &date1.day, &date1.year);
+    printf("Enter date #1(mm/dd/yyyy): \n");
+    scanf("%i/%i/%i", &date2.month, &date2.day, &date2.year);
+    
+    printf("%i\n", calcDays(date2) - calcDays(date1));
+}
