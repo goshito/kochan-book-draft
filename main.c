77f324cd89d556694d9a5035d4f1b18bd1f411db
georgi <georgi@georgi-Aspire-E5-572G>
2016-05-04 15:07:09 +0300

    dictionary sort

--- main.c
+++ main.c
@@ -1,76 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: georgi
- *
- * Created on April 21, 2016, 5:06 PM
- */
-
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 #include <string.h>
 
-/*
- * Chapter 10, Exercise 7
- * 
- * Write a function called insertString to insert one character string into
- * another string. The arguments to the function should consist of the source
- * string, the string to be inserted, and the position in the source string
- * where the string is to be inserted. So the call
- * 
- * insertString(text, "per", 10);
- * 
- * with text as originally defined in the previous exercise, results in the
- * character string "per" being inserted inside text, beginning at text[10]. 
- * Therefore, the character
- * string "the wrong person" is stored inside the text array after the function
- * returned.
- */
-
-void insertString(char text[], char insert[], int startPos) {
-    int  endPos, i, j;
-    char nameBuff[strlen(text) + strlen(insert) + 1];
-    
-    i = 0;
-    while (i < startPos) {
-        nameBuff[i] = text[i];
-        i++;
+int main(){
+    int i,j;
+    char str[10][50],temp[50];
+    printf("Enter 10 words:\n");
+    for(i=0;i<10;++i)
+        gets(str[i]);
+    for(i=0;i<9;++i)
+       for(j=i+1;j<10 ;++j){
+          if(strcmp(str[i],str[j])>0)
+          {
+            strcpy(temp,str[i]);
+            strcpy(str[i],str[j]);
+            strcpy(str[j],temp);
+          }
     }
-    
-    endPos = startPos + strlen(insert);
-    
-    j = 0;
-    while (i < endPos) {
-        nameBuff[i] = insert[j];
-        i++;
-        j++;
+    printf("In lexicographical order: \n");
+    for(i=0;i<10;++i){
+       puts(str[i]);
     }
-    
-    while (i < strlen(text) + sizeof(insert)) {
-        nameBuff[i] = text[i - strlen(insert)];
-        i++;
-    }
-    
-    // allocate space for the new characters
-    text = (char*) malloc(strlen(insert) + 1);
-    
-    // copy result back to initial string    
-    strcpy(text, nameBuff);
-    //printf("%s", text);
-    
-    free(text);
+return 0;
 }
-
-int main(int argc, char** argv) {
-    char test[81] = "georgitoshev";
-    
-    insertString(test, "val", 6);
-    printf("%s", test);
-    return (EXIT_SUCCESS);
-}
-
