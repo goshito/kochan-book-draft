d25ac94e8da6264ce4773b8441d962e738daab0e
georgi <georgi@georgi-Aspire-E5-572G>
2016-05-16 17:00:46 +0300

    Ex. 10-12 after removing loops

--- main.c
+++ main.c
@@ -15,10 +15,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <time.h>
 
 /*
- * Chapter 10, Exercise 12
+ * !!! the smaller for loop was removed
+ * 
+ * Chapter 10, Exercise 12 Attempting to DRY 14052016 1857, 15052016 1504,1712
  * 
  * Write a function called strToFloat that converts a character string into a
  * floating point value. Have the function accept an optional leading minus.
@@ -29,75 +32,51 @@
  * should return the value -867.6921
  */
 
-// flag the start of the decimal point !!!???
 
-double strToFloat(char string[]) { // type cast???
-    int i, j, decpnt;
+
+double strToFloat(char string[]) { 
+    int i = 0, decpnt;
     double floatValue, result = 0;
-    
-    // handle minus
-    if (string[0] == '-') {
-       for (i = 1; string[i] >= '.' && string[i] <= '9'; ++i) {
-           if (string[i] == '/') { //skip the backslash
-                i++;
-                continue;
-            } else if (string[i] == '.') { //skip the decimal point
-                decpnt = i - 1; // mark start of decimal point(-1 because for i = 1)
-                continue;
-            }    
-           
-           floatValue = string[i] - '0';
-           result = result * 10 + floatValue;  
-       }
-       
-       for (j = 0; result >= 1; j++) {
-            result /= 10;
-        }
-        
-        for (int m = 0; m < decpnt; m++) {
-            result *= 10;
-        } 
-       
-       return -result;
-    } else {                  // calculate positive floating point number      
-        for (i = 0; string[i] >= '.' && string[i] <= '9'; ++i) {
-            
-            if (string[i] == '/') { //skip the backslash
-                i++;
-                continue;
-            } else if (string[i] == '.') { //skip the decimal point
-                decpnt = i;     // from where decimal point starts
-                continue;
+
+    /* handle minus and plus */
+    if (string[0] == '-' || string[0] == '+')
+        i++;
+    /* convert string to int*/
+    for (; (string[i] >= '0' && string[i] <= '9') || string[i] == '.'; i++) {
+
+        if (string[i] == '.') { /* skip the decimal and start dividing by 10 */
+            if (string[0] == '-' || string[0] == '+') {
+                decpnt = i - 1; /* mark start of decimal point(-1 because for i = 1)*/
+            } /*divide the number n times*/
+            else {                
+                decpnt = i;
             }
-            floatValue = string[i] - '0';
-            result = result * 10 + floatValue; 
+            continue;
         }
-        // 
-        for (j = 0; result >= 1; j++) {  
-            result /= 10;
-        }
-        // place the decimal point
-        for (int m = 0; m < decpnt; m++) {
-            result *= 10;
-        }        
+
+        floatValue = string[i] - '0';
+        result = result * 10 + floatValue;
         
-        //insert decimal point in appropriate place
-       return result; 
-    }    
+    }
     
+    if (string[0] == '+' || string[0] == '-')
+        i--;
+    
+	int x = i - decpnt - 1;
+	result = result * 1/(pow(10, x));    
+
+    if (string[0] == '-')
+        return -result;
+    else
+        return result;
+
 }
 
 int main(int argc, char** argv) {
     
-    for (int i = 0; i < 100000; i++) {
-        printf("%f", strToFloat("123456.78\n")); // working
-    }
-    
+    for (int i = 0; i < 100000; i++)
+        printf("%f\n", strToFloat("123456.78"));   
+
     return (EXIT_SUCCESS);
 }
 
-/*
- * Ideas:
- * 
- * 1. Decimal count isn't used 
- */
