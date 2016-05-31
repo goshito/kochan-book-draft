cdfef1523659c3a4d8e341e96f6735aefe5f96b6
georgi <georgi@georgi-Aspire-E5-572G>
2016-05-31 16:39:06 +0300

    Totally mixed up Exercise 11-3. HELP!

--- test.c
+++ test.c
@@ -1,15 +1,84 @@
-#include <stdio.h>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-int main(void) {
-    int count = 10;
-    int *int_pointer; // define a variable which is a pointer to int
-    /*setup the indirect reference between int_pointer and count.
-     Assign to int_pointer not the variable count but a pointer to it*/
-    int_pointer = &count;
-    int x;
+/* Chapter 11, Exercise 3
+ * File:   insEntryFron.c
+ * Author: georgi
+ *
+ * Created on May 30, 2016, 5:09 PM
+ * 3. The function developed in exercise 2 only inserts an element after an existing ele-
+ment in the list, thereby preventing you from inserting a new entry at the front of
+the list. How can you use this same function and yet overcome this problem?
+(Hint: Think about setting up a special structure to point to the beginning of the
+list.)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * must fix displayed results
+ * how to change list pointer to point to the new
+ * 
+ */
+struct entry {
+    int value;
+    struct entry *next;
+};
+
+int main(int argc, char** argv) {
+    void insertEntry(struct entry *insert, struct entry *where);
+    void printList(struct entry *list_pointer);
     
-    x = *int_pointer;
-    printf("%i\n", x);
+    //my structures and a pointer to the beginning of the list
+    struct entry n1, n2, n3, *list_start = &n1;
+    //this will be inserted at the beginning of the list   
+    struct entry n0, *ptr_n0 = &n0, *ptr_n1 = &n1;
     
-    return 0;
+    
+    n0.value = 50;
+    n0.next = &n1;
+    
+    n1.value = 100;
+    n1.next = &n2;
+    
+    n2.value = 200;
+    n2.next = &n3;
+    
+    n3.value = 300;
+    n3.next = (struct entry *) 0;
+    
+    //display the linked list before insert
+    printf("Linked list before insert:"); 
+    printList(list_start);
+    //insert n21 after n2
+    insertEntry(new_first, old_first);
+    //display the linked list
+    printf("\nLinked list after insert:");
+    printList(list_start);
+
+    return (EXIT_SUCCESS);
+}
+
+//function to insert an entry into the linked list's beginning
+void insertEntry(struct entry *insert, struct entry *after) {
+    
+    insert->value = 50;
+    insert->next = after->next;
+    after->next = insert;
+    
+}
+//function to display the contents of the linked list
+void printList(struct entry *list_pointer) {
+    
+    while (list_pointer != (struct entry *) 0) {
+        printf("%i", list_pointer->value);
+        list_pointer = list_pointer->next;
+        if (list_pointer)
+            printf(",");
+    }
+    
 }
